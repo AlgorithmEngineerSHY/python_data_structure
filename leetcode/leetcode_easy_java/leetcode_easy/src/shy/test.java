a32a20fab2800166368e998636d2c55c25bdf10d
AlgorithmEngineerSHY <13141462249@163.com>
2017-09-05 19:28:43 +0800

    Update test.java

--- leetcode/leetcode_easy_java/leetcode_easy/src/shy/test.java
+++ leetcode/leetcode_easy_java/leetcode_easy/src/shy/test.java
@@ -6,7 +6,7 @@ public class test {
 		// TODO Auto-generated method stub
 		System.out.println("好想崔朔啊！");
 		System.out.println("lalala");
-		System.out.println("lalala");
+		System.out.println("lalalala");
 	}
 
 }
